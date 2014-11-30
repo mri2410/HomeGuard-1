7a956ebb7bb92223c377fd0ffe9aa1bd3a80ed28
jianxiapyh <pyihan1@vt.edu>
2014-11-30 13:38:13 -0800

    Update getSensorData.py
    adding the sensor collection

--- getSensorData.py
+++ getSensorData.py
@@ -5,6 +5,48 @@ import signal
 import os
 import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23
+ECHO = 24
+#assign pins
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+signal.signal(signal.SIGUSR1,restart)
+waitstart=time.time()
+def collectsensordata():
+	print "Program Started"
+ 	try:
+		pause=0
+		while 1:
+			#reset the sensor
+			GPIO.output(TRIG, False)
+			time.sleep(2) 
+			#trigger the sensor
+			GPIO.output(TRIG, True)
+			time.sleep(0.00001)
+			GPIO.output(TRIG, False)	
+			
+			while GPIO.input(ECHO)==0:
+				pulse_start = time.time()
+			while GPIO.input(ECHO)==1:
+				pulse_end = time.time() 
+			pulse_duration = pulse_end - pulse_start
+			distance = pulse_duration * 17150
+			distance = round(distance, 2)
+			if pause==0 and distance>2:
+				pause=1 
+				waitstart=time.time()
+			elif pause==1 and distance>400:
+				pause=0
+			elif time.time()-waitstart>300:
+				pause=0				
+
+			
+	except KeyboardInterrupt: 
+		GPIO.cleanup()
+
+
 def getSensorData(object):
 	while object.getLoopState():
 		time.sleep(1)
