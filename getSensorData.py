add167eeced6a052e9dff0b3508fa2ebaefeee4f
Arun Rai <raiarun52@gmail.com>
2014-11-30 17:55:04 -0500

    Update getSensorData.py

--- getSensorData.py
+++ getSensorData.py
@@ -18,7 +18,7 @@ waitstart=time.time()
 def getSensorData(object):
  	try:
 		pause=0
-		while 1:
+		while object.getLoopState():
 			#reset the sensor
 			GPIO.output(TRIG, False)
 			time.sleep(2) 
@@ -26,7 +26,6 @@ def getSensorData(object):
 			GPIO.output(TRIG, True)
 			time.sleep(0.00001)
 			GPIO.output(TRIG, False)	
-			
 			while GPIO.input(ECHO)==0:
 				pulse_start = time.time()
 			while GPIO.input(ECHO)==1:
@@ -43,6 +42,12 @@ def getSensorData(object):
 				message['type']="trigger"
 				object.setMessage(message)
 				object.setMessageSignal(True)
+			elif pause==1 and distance>400:
+				pause=0
+			elif time.time()-waitstart>300:
+				pause=0				
+
+			if object.getMessageSignal() == True:
 				message = json.dumps(object.getMessage(), indent = 2)
 				MessageBroker = pika.BlockingConnection(pika.ConnectionParameters(host = '127.0.0.1',
 											virtual_host="mycomputer",
@@ -58,12 +63,6 @@ def getSensorData(object):
 				""" Close the connection """ 
 				MessageBroker.close()
 				object.setMessageSignal(False)
-
-			elif pause==1 and distance>400:
-				pause=0
-			elif time.time()-waitstart>300:
-				pause=0				
-
 			
 	except KeyboardInterrupt: 
 		GPIO.cleanup()
